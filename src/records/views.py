0e02239e98c3ab9846fcf7e6c8781fc4a11ad704
mjtamlyn <marc.tamlyn@gmail.com>
2011-06-13 19:33:11 +0100

    some very basic css, static files working

--- src/records/views.py
+++ src/records/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 
 from records.models import *
 
 def index(request):
     competitions = Competition.objects.all()
-    return render_to_response('index.html', locals())
+    return render_to_response('index.html', locals(), context_instance=RequestContext(request))
 
 def competition_index(request, competition_id):
     competition = get_object_or_404(Competition, id=competition_id)
     results = competition.full_results()
-    return render_to_response('competition_index.html', locals())
+    return render_to_response('competition_index.html', locals(), context_instance=RequestContext(request))
--- src/urls.py
+++ src/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 
 from django.contrib import admin
@@ -7,3 +8,8 @@ urlpatterns = patterns('',
     (r'', include('records.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import urlpatterns as static_patterns
+    urlpatterns += static_patterns
+
