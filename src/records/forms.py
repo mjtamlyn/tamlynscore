83c074dd7c5c9783a0af9e0656d90a37ae370508
mjtamlyn <marc.tamlyn@gmail.com>
2011-06-14 16:12:02 +0100

    colorscheme, some css work

--- src/records/forms.py
+++ src/records/forms.py
@@ -2,16 +2,9 @@ from django import forms
 
 from records.models import *
 
-class SmartSearchField(forms.ComboField):
-    fields = [forms.Field, forms.ChoiceField]
-
 class ShootForm(forms.ModelForm):
-
-    competition = forms.models.ModelChoiceField(Competition.objects.all(), widget=forms.widgets.HiddenInput)
-    archer = SmartSearchField()
-    club = SmartSearchField()
-    bowstyle = forms.models.ModelChoiceField(Bowstyle.objects.all(), widget=forms.widgets.RadioSelect(), initial=1)
 
     class Meta:
         model = Shoot
+        exclude = ['competition']
 
