f72caaf833498fb3c31f616723e9efb4afb64fd0
mjtamlyn <marc.tamlyn@gmail.com>
2011-06-16 18:56:18 +0100

    search progress

--- src/records/static/js/add_scores.js
+++ src/records/static/js/add_scores.js
@@ -1,14 +1,17 @@
 var SmartInput = new Class({
     setUp: function () {
         $$('.smart-input').each(function (item, index, array) {
-            var input, select, search;
+            var input, select, search, hits, resultsLookup;
             input = item.getElement('input[type=text]');
             select = item.getElement('select');
             search = {};
+            hits = {};
+            resultsLookup = {};
             select.getElements('option').each(function (option) {
                 var name, value;
                 value = option.get('value');
                 if (value) {
+                    hits[value] = 0;
                     name = option.get('html'); 
                     name.split(' ').each(function (part) {
                         if (search[part]) {
@@ -17,15 +20,41 @@ var SmartInput = new Class({
                             search[part] = [value];
                         }
                     });
+                    resultsLookup[value] = name;
                 }
             });
+            console.log(resultsLookup);
             input.addEvent('keyup', function (e) {
                 if (e.key == 'enter' || e.key == 'up' || e.key == 'down') {
                     console.log('hello');
                     e.stop();
+                    return;
                 }
                 var params = this.value.split(' ');
                 var searchList = Object.keys(search);
+                Object.keys(hits).each(function (item) {
+                    hits[item] = 0;
+                });
+                params.each(function (param) {
+                    if (param === '') {
+                        params.pop(param);
+                        return;
+                    }
+                    searchList.each(function (searchTerm) {
+                        if (RegExp('^' + param, 'i').test(searchTerm)) {
+                            search[searchTerm].each(function (i) {
+                                hits[i] += 1;
+                            });
+                        }
+                    });
+                });
+                var results = [];
+                Object.keys(hits).each(function (item, index) {
+                    if (hits[item] === params.length) {
+                        results.push(index);
+                    }
+                });
+                console.log(results);
             });
         });
     },
