4217b7e548de67db5b3455249d50c799d9eb69d6
mjtamlyn <marc.tamlyn@gmail.com>
2011-06-18 10:21:08 +0100

    Uniqueness conditions, initial data fixture.

---
+++ src/records/migrations/0009_auto__add_unique_bowstyle_name__add_unique_competition_date_tournament.py
@@ -0,0 +1,133 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding unique constraint on 'Bowstyle', fields ['name']
+        db.create_unique('records_bowstyle', ['name'])
+
+        # Adding unique constraint on 'Competition', fields ['date', 'tournament']
+        db.create_unique('records_competition', ['date', 'tournament_id'])
+
+        # Adding unique constraint on 'Club', fields ['name']
+        db.create_unique('records_club', ['name'])
+
+        # Adding unique constraint on 'Entry', fields ['archer', 'shot_round']
+        db.create_unique('records_entry', ['archer_id', 'shot_round_id'])
+
+        # Adding unique constraint on 'Archer', fields ['club', 'name']
+        db.create_unique('records_archer', ['club_id', 'name'])
+
+        # Adding unique constraint on 'Arrow', fields ['entry', 'arrow_of_round', 'subround']
+        db.create_unique('records_arrow', ['entry_id', 'arrow_of_round', 'subround_id'])
+
+        # Adding unique constraint on 'Tournament', fields ['full_name']
+        db.create_unique('records_tournament', ['full_name'])
+
+
+    def backwards(self, orm):
+        
+        # Removing unique constraint on 'Tournament', fields ['full_name']
+        db.delete_unique('records_tournament', ['full_name'])
+
+        # Removing unique constraint on 'Arrow', fields ['entry', 'arrow_of_round', 'subround']
+        db.delete_unique('records_arrow', ['entry_id', 'arrow_of_round', 'subround_id'])
+
+        # Removing unique constraint on 'Archer', fields ['club', 'name']
+        db.delete_unique('records_archer', ['club_id', 'name'])
+
+        # Removing unique constraint on 'Entry', fields ['archer', 'shot_round']
+        db.delete_unique('records_entry', ['archer_id', 'shot_round_id'])
+
+        # Removing unique constraint on 'Club', fields ['name']
+        db.delete_unique('records_club', ['name'])
+
+        # Removing unique constraint on 'Competition', fields ['date', 'tournament']
+        db.delete_unique('records_competition', ['date', 'tournament_id'])
+
+        # Removing unique constraint on 'Bowstyle', fields ['name']
+        db.delete_unique('records_bowstyle', ['name'])
+
+
+    models = {
+        'records.archer': {
+            'Meta': {'unique_together': "(('name', 'club'),)", 'object_name': 'Archer'},
+            'bowstyle': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.Bowstyle']"}),
+            'club': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.Club']"}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'records.arrow': {
+            'Meta': {'unique_together': "(('subround', 'arrow_of_round', 'entry'),)", 'object_name': 'Arrow'},
+            'arrow_of_round': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'entry': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.Entry']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'score': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'subround': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.Subround']"})
+        },
+        'records.boundround': {
+            'Meta': {'object_name': 'BoundRound'},
+            'competition': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.Competition']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'round_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.Round']"})
+        },
+        'records.bowstyle': {
+            'Meta': {'object_name': 'Bowstyle'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        'records.club': {
+            'Meta': {'object_name': 'Club'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '500'}),
+            'short_name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'records.competition': {
+            'Meta': {'unique_together': "(('date', 'tournament'),)", 'object_name': 'Competition'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'rounds': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['records.Round']", 'through': "orm['records.BoundRound']", 'symmetrical': 'False'}),
+            'tournament': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.Tournament']"})
+        },
+        'records.entry': {
+            'Meta': {'unique_together': "(('archer', 'shot_round'),)", 'object_name': 'Entry'},
+            'archer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.Archer']"}),
+            'bowstyle': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.Bowstyle']"}),
+            'club': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.Club']"}),
+            'golds': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'hits': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'score': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'shot_round': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['records.BoundRound']"})
+        },
+        'records.round': {
+            'Meta': {'object_name': 'Round'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'subrounds': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['records.Subround']", 'symmetrical': 'False'})
+        },
+        'records.subround': {
+            'Meta': {'object_name': 'Subround'},
+            'arrows': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'distance': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'unit': ('django.db.models.fields.CharField', [], {'max_length': '1'})
+        },
+        'records.tournament': {
+            'Meta': {'object_name': 'Tournament'},
+            'full_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '300'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'short_name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        }
+    }
+
+    complete_apps = ['records']
