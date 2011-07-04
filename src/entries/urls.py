8dc5dad1a2804ab243f10a0511ee0ef37000f49c
mjtamlyn <marc.tamlyn@gmail.com>
2011-07-04 19:20:53 +0100

    Entries: a couple of simple navigational views

--- src/entries/urls.py
+++ src/entries/urls.py
@@ -3,11 +3,7 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('records.views',
-    (r'^$', 'index'),
-    (r'^competition/(?P<slug>(\w+-?)+)/$', 'competition_index'),
-    (r'^add-scores/(?P<round_id>\d+)/$', 'add_scores'),
-    (r'^add-arrow-values/(?P<round_id>\d+)/$', 'add_arrow_values_index'),
-    (r'^add-arrow-values/(?P<round_id>\d+)/target/(?P<target_no>\d+)/doz/(?P<doz_no>\d+)/$', 'add_arrow_values'),
-    (r'^new-club/$', 'new_club'),
+urlpatterns = patterns('entries.views',
+    (r'^$', 'tournaments'),
+    (r'^(?P<slug>[\w-]+)$', 'competition_index'),
 )
