b68fa3817ea8607ea6e19cc9dcca50afa19ef0ce
mjtamlyn <marc.tamlyn@gmail.com>
2011-07-15 13:54:04 +0100

    Registration page

---
+++ src/entries/migrations/0004_auto__add_field_sessionentry_present.py
@@ -0,0 +1,117 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'SessionEntry.present'
+        db.add_column('entries_sessionentry', 'present', self.gf('django.db.models.fields.BooleanField')(default=False), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'SessionEntry.present'
+        db.delete_column('entries_sessionentry', 'present')
+
+
+    models = {
+        'core.archer': {
+            'Meta': {'object_name': 'Archer'},
+            'age': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'bowstyle': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Bowstyle']"}),
+            'club': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Club']"}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'gnas_no': ('django.db.models.fields.BigIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'novice': ('django.db.models.fields.CharField', [], {'max_length': '1'})
+        },
+        'core.bowstyle': {
+            'Meta': {'object_name': 'Bowstyle'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        'core.club': {
+            'Meta': {'object_name': 'Club'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '500'}),
+            'short_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'})
+        },
+        'core.round': {
+            'Meta': {'object_name': 'Round'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'scoring_type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'subrounds': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.Subround']", 'symmetrical': 'False'})
+        },
+        'core.subround': {
+            'Meta': {'object_name': 'Subround'},
+            'arrows': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'distance': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'target_face': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'unit': ('django.db.models.fields.CharField', [], {'max_length': '1'})
+        },
+        'entries.competition': {
+            'Meta': {'unique_together': "(('date', 'tournament'),)", 'object_name': 'Competition'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'end_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'tournament': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entries.Tournament']"})
+        },
+        'entries.competitionentry': {
+            'Meta': {'object_name': 'CompetitionEntry'},
+            'age': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'archer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Archer']"}),
+            'bowstyle': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Bowstyle']"}),
+            'club': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Club']"}),
+            'competition': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entries.Competition']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'novice': ('django.db.models.fields.CharField', [], {'max_length': '1'})
+        },
+        'entries.session': {
+            'Meta': {'object_name': 'Session'},
+            'archers_per_target': ('django.db.models.fields.IntegerField', [], {}),
+            'competition': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entries.Competition']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'scoring_system': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
+            'start': ('django.db.models.fields.DateTimeField', [], {})
+        },
+        'entries.sessionentry': {
+            'Meta': {'object_name': 'SessionEntry'},
+            'competition_entry': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entries.CompetitionEntry']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'present': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'session_round': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entries.SessionRound']"})
+        },
+        'entries.sessionround': {
+            'Meta': {'object_name': 'SessionRound'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'session': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entries.Session']"}),
+            'shot_round': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Round']"})
+        },
+        'entries.targetallocation': {
+            'Meta': {'object_name': 'TargetAllocation'},
+            'boss': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'session_entry': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['entries.SessionEntry']"}),
+            'target': ('django.db.models.fields.CharField', [], {'max_length': '1'})
+        },
+        'entries.tournament': {
+            'Meta': {'object_name': 'Tournament'},
+            'full_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '300'}),
+            'host_club': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Club']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'short_name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        }
+    }
+
+    complete_apps = ['entries']
