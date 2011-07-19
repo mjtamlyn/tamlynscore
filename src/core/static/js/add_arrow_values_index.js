6761ee0fc159fbf16aae034a7561fee525a8d4cd
mjtamlyn <marc.tamlyn@gmail.com>
2011-07-19 15:39:39 +0100

    Cleanup

--- src/core/static/js/add_arrow_values_index.js
+++
@@ -1,8 +0,0 @@
-var ValuesIndexPage = new Class({
-    setUp: function () {
-        focusElement = $$('.focus')[0]
-        if (focusElement) {
-            focusElement.focus();
-        }
-    }
-});
