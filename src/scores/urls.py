2bd250c3460d33ccce0b7eaef217449f9d181a2b
mjtamlyn <marc.tamlyn@gmail.com>
2011-07-25 21:32:01 +0100

    Basic leaderboard

--- src/scores/urls.py
+++ src/scores/urls.py
@@ -6,4 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('scores.views',
     (r'^(?P<slug>(\w+-?)+)/input-scores/$', 'input_scores'),
     (r'^(?P<slug>(\w+-?)+)/input-arrows/(?P<round_id>\d+)/doz(?P<dozen>\d+)/boss(?P<boss>\d+)/$', 'input_arrows'),
+    (r'^(?P<slug>(\w+-?)+)/leaderboard/$', 'leaderboard'),
 )
