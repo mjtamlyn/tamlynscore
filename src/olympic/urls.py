8d003e2dadff7b9bbe4b1d93e2b792b8f1bd8d61
Marc Tamlyn <marc@incuna.com>
2011-08-07 14:32:26 +0100

    H2H Results pdf.

--- src/olympic/urls.py
+++ src/olympic/urls.py
@@ -6,4 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('olympic.views',
     (r'^(?P<slug>[\w-]+)/$', 'olympic_index'),
     (r'^(?P<slug>[\w-]+)/score-sheets/(?P<round_id>\d+)/$', 'olympic_score_sheet'),
+    (r'^(?P<slug>[\w-]+)/results/$', 'olympic_results'),
 )
