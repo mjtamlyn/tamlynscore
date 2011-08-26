5b63af3d83c537b557ed17f8ca1a6c8ba5683a18
Marc Tamlyn <marc@incuna.com>
2011-08-26 21:31:18 +0100

    Merge.

--- src/olympic/urls.py
+++ src/olympic/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls.defaults import *
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('olympic.views',
     (r'^(?P<slug>[\w-]+)/$', 'olympic_index'),
     (r'^(?P<slug>[\w-]+)/score-sheets/(?P<round_id>\d+)/$', 'olympic_score_sheet'),
