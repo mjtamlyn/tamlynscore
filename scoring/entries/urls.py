ee2ed9dc8280f3a532bcdeb226c63e1bbd457e2b
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-02-03 20:03:30 +0000

    Convert score sheets index to cbv.

--- scoring/entries/urls.py
+++ scoring/entries/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('entries.views',
     url(r'^(?P<slug>[\w-]+)/target-list/$', views.BetterTargetList.as_view(), name='target_list'),
     (r'^(?P<slug>[\w-]+)/target-list/pdf/$', 'target_list_pdf'),
     (r'^(?P<slug>[\w-]+)/target-list/lunch/$', 'target_list_lunch'),
-    (r'^(?P<slug>[\w-]+)/score-sheets/$', 'score_sheets'),
+    url(r'^(?P<slug>[\w-]+)/score-sheets/$', views.ScoreSheets.as_view(), name='score_sheets'),
     (r'^(?P<slug>[\w-]+)/score-sheets/(?P<round_id>\d+)/$', 'score_sheets_pdf'),
     (r'^(?P<slug>[\w-]+)/running-slips/(?P<round_id>\d+)/$', 'running_slips_pdf'),
 
