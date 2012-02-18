8723b478990749aeaa3199b80e6b1f23b441cea3
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-02-18 07:22:54 +0000

    Add table view for club admin.

--- scoring/core/admin.py
+++ scoring/core/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 
 from core.models import *
 
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_name', 'abbreviation')
+
 admin.site.register(Country)
 admin.site.register(Region)
 admin.site.register(County)
-admin.site.register(Club)
+admin.site.register(Club, ClubAdmin)
 admin.site.register(Archer)
 admin.site.register(Bowstyle)
 admin.site.register(Round)
