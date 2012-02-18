8c1a73b7e20c171836a87d8673191ff24fcc54db
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-02-18 08:07:25 +0000

    Genders on import script.

--- scoring/import_bucs.py
+++ scoring/import_bucs.py
@@ -9,29 +9,37 @@ session_rounds = SessionRound.objects.filter(session__competition=competition)
 
 clubs = set()
 total = 0
-for session_name, session_round in zip(['A', 'B', 'C'], session_rounds):
+for session_name, session_round in zip(['A'], session_rounds):
     file_name = '/home/marc/Desktop/BUSA%s.csv' % session_name
     f = open(file_name, 'r')
     reader = csv.reader(f)
     rows = filter(lambda r: r[1], reader)
-    rows = [row[:5] for row in rows]
-    for target, archer, bow, club, exp in rows:
-        bowstyle = Bowstyle.objects.get(name__iexact=bow)
+    rows = [row[:7] for row in rows]
+    for session, target, archer, bow, club, gender, exp in rows:
+        #bowstyle = Bowstyle.objects.get(name__iexact=bow)
         #if exp[0] not in ['E', 'N']:
         #    print exp
-        exp = exp[0]
-        club = Club.objects.get(short_name__iexact=club.strip() + ' Uni')
+        #exp = exp[0]
+        if gender == 'Male':
+            gender = 'G'
+        elif gender == 'Female':
+            gender = 'L'
+        #club = Club.objects.get(short_name__iexact=club.strip() + ' Uni')
         # get or create archer
         try:
-            archer = Archer.objects.get(name__iexact=archer, club=club)
+            archer = Archer.objects.get(name__iexact=archer)
         except Archer.DoesNotExist:
-            archer = Archer(name=archer, club=club, novice=exp, age='S', bowstyle=bowstyle, gender='G')
-            archer.save()
+            print archer
+            continue
+            #archer = Archer(name=archer, club=club, novice=exp, age='S', bowstyle=bowstyle, gender='G')
+            #archer.save()
+        archer.gender = gender
+        archer.save()
         # get or create competition entry
-        competition_entry, created = CompetitionEntry.objects.get_or_create(competition=competition, archer=archer, bowstyle=bowstyle, novice=exp, age='S', club=club)
-        session_entry, created = SessionEntry.objects.get_or_create(competition_entry=competition_entry, session_round=session_round)
-        target_allocation, created = TargetAllocation.objects.get_or_create(session_entry=session_entry, boss=target[:-1], target=target[-1])
-        print target[:-1], target[-1], archer
+        #competition_entry, created = CompetitionEntry.objects.get_or_create(competition=competition, archer=archer, bowstyle=bowstyle, novice=exp, age='S', club=club)
+        #session_entry, created = SessionEntry.objects.get_or_create(competition_entry=competition_entry, session_round=session_round)
+        #target_allocation, created = TargetAllocation.objects.get_or_create(session_entry=session_entry, boss=target[:-1], target=target[-1])
+        #print target[:-1], target[-1], archer
 
 
 #clubs = sorted(list(clubs))
