5b24dff583aec11abcfc3b656021c622377ca3a2
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-02-18 07:52:00 +0000

    Archer admin.

--- scoring/core/admin.py
+++ scoring/core/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from core.models import *
 
+
+class ArcherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'club')
+    list_filter= ('club',)
+
 class ClubAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_name', 'abbreviation')
 
@@ -9,7 +14,7 @@ admin.site.register(Country)
 admin.site.register(Region)
 admin.site.register(County)
 admin.site.register(Club, ClubAdmin)
-admin.site.register(Archer)
+admin.site.register(Archer, ArcherAdmin)
 admin.site.register(Bowstyle)
 admin.site.register(Round)
 admin.site.register(Subround)
