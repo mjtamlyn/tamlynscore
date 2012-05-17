8563ab08451b5beba706426ef6b3756cb247d8b2
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-05-17 18:10:18 +0100

    Display headers on the results pdf.

--- scores/views.py
+++ scores/views.py
@@ -12,11 +12,12 @@ from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
 from django.views.generic import View, ListView
 
+from reportlab.lib import colors
 from reportlab.lib.units import inch
-from reportlab.platypus import PageBreak, Table
+from reportlab.platypus import PageBreak, Table, TableStyle
 
 from entries.models import Competition, SessionRound, SCORING_TOTALS, SCORING_DOZENS, SCORING_FULL
-from entries.views import TargetList, HeadedPdfView, BetterTargetList
+from entries.views import HeadedPdfView, BetterTargetList
 
 from scores.forms import get_arrow_formset, get_dozen_formset
 from scores.models import Score, Arrow, Dozen
@@ -389,31 +390,28 @@ class ResultsPdf(HeadedPdfView, ResultsView):
         self.styles['h1'].alignment = 1
         self.styles['h2'].alignment = 1
 
-    def row_from_entry(self, entries, entry):
+    def row_from_entry(self, entries, entry, subrounds):
         row = [
             entries.index(entry) + 1 if not entry.disqualified else None,
             entry.target.session_entry.competition_entry.archer.name,
             entry.target.session_entry.competition_entry.club.name,
         ]
-        # TODO: Denormalise this (or something!)
-        subrounds = entry.target.session_entry.session_round.shot_round.subrounds
-        num_subrounds = subrounds.count()
-        if num_subrounds > 1:
+        if len(subrounds) > 1:
             if entry.disqualified or entry.target.session_entry.session_round.session.scoring_system == SCORING_TOTALS:
-                row += [None] * num_subrounds
+                row += [None] * len(subrounds)
             else:
                 subround_scores = []
 
                 if entry.target.session_entry.session_round.session.scoring_system == SCORING_FULL:
                     # Arrow of round has been stored off by a dozen
                     counter = 13
-                    for subround in subrounds.order_by('-distance'):
+                    for subround in subrounds:
                         subround_scores.append(entry.arrow_set.filter(arrow_of_round__in=range(counter, counter + subround.arrows)).aggregate(models.Sum('arrow_value'))['arrow_value__sum'])
                         counter += subround.arrows
 
                 elif entry.target.session_entry.session_round.session.scoring_system == SCORING_DOZENS:
                     counter = 1
-                    for subround in subrounds.order_by('-distance'):
+                    for subround in subrounds:
                         subround_scores.append(entry.dozen_set.filter(dozen__in=range(counter, counter + subround.arrows / 12)).aggregate(models.Sum('total'))['total__sum'])
                         counter += subround.arrows / 12
 
@@ -440,8 +438,16 @@ class ResultsPdf(HeadedPdfView, ResultsView):
             elements.append(self.Para(session_round.shot_round, 'h1'))
 
             for category, entries in results:
+                if not entries:
+                    continue
                 elements.append(self.Para(category , 'h2'))
-                table_data = [self.row_from_entry(entries, entry) for entry in entries]
+                table_header = ['Pl.', 'Archer', 'Club']
+                subrounds = entries[0].target.session_entry.session_round.shot_round.subrounds.order_by('-distance')
+                if len(subrounds) > 1:
+                    table_header += ['{0}{1}'.format(subround.distance, subround.unit) for subround in subrounds]
+                table_header += ['Score', '10s', 'Xs']
+                table_data = [table_header]
+                table_data += [self.row_from_entry(entries, entry, subrounds) for entry in entries]
                 if table_data:
                     gender = entries[0].target.session_entry.competition_entry.archer.gender
                     bowstyle = entries[0].target.session_entry.competition_entry.bowstyle
@@ -457,10 +463,15 @@ class ResultsPdf(HeadedPdfView, ResultsView):
                     #FIXME! DNS for a category where everyone shot...
                     pass
                 table = Table(table_data)
+                table.setStyle(self.table_style)
                 elements.append(table)
             
             elements.append(PageBreak())
 
         return elements
+
+    table_style = TableStyle([
+        ('LINEBELOW', (0, 0), (-1, 0), 0.5, colors.black),
+    ])
 
 results_pdf = login_required(ResultsPdf.as_view())
