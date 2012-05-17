2f27ca380cbe1ef2f51d10f9cdbdcfa583a89cb6
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-05-17 09:42:12 +0100

    Simple admin for Scores.
    fix the abysmal page load issues!

--- scores/admin.py
+++ scores/admin.py
@@ -7,6 +7,13 @@ class ArrowAdmin(admin.ModelAdmin):
     list_display = ('arrow_value', 'score')
 
 
-admin.site.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    raw_id_fields = ('target',)
+
+    def queryset(self, request):
+        return super(ScoreAdmin, self).queryset(request).select_related()
+
+
+admin.site.register(Score, ScoreAdmin)
 admin.site.register(Arrow, ArrowAdmin)
 admin.site.register(Dozen)
