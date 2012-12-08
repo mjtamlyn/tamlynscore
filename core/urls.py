d34e6159c7cb40dc894b905b585dc14557de19a8
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-12-08 16:12:55 +0000

    Remove some pointless variables.

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ import views
 
 urlpatterns = patterns('core.views',
     url(r'^$', views.Index.as_view(), name='index'),
-    url(r'^clubs/$', 'club_list', name='club_list'),
-    url(r'^clubs/(?P<slug>[\w-]+)/$', 'club_detail', name='club_detail'),
+    url(r'^clubs/$', views.ClubList.as_view(), name='club_list'),
+    url(r'^clubs/(?P<slug>[\w-]+)/$', views.ClubDetail.as_view(), name='club_detail'),
     url(r'^archer/(?P<pk>\d+)/$', views.ArcherUpdate.as_view(), name='archer_update'),
 )
