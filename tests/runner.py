58f5b2b4532fa2515736fdfb8d8427ec6d0e59ba
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-12-08 17:24:57 +0000

    Add test runner to speed up tests.

---
+++ tests/runner.py
@@ -0,0 +1,14 @@
+import logging
+
+from django.conf import settings
+
+from discover_runner import DiscoverRunner
+
+
+class ScoringRunner(DiscoverRunner):
+    def setup_test_environment(self):
+        # No logging thanks
+        logging.disable(logging.CRITICAL)
+        super(ScoringRunner, self).setup_test_environment()
+        # Use a faster password hasher
+        settings.PASSWORD_HASHERS = ('django.contrib.auth.hashers.MD5PasswordHasher',)
