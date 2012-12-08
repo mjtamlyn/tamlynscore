4ee631ca3bbba8cc43197ba9be230d05644d0fb6
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-12-08 17:06:55 +0000

    Add the very first real test.

---
+++ tests/test_non_tournament_pages.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+
+from . import factories
+
+
+class BasicPageTest(object):
+    url_name = None
+    responses = None
+    login_required = False
+
+    def setUp(self):
+        self.client = Client()
+
+    def login(self):
+        user = factories.UserFactory.create()
+        logged_in = self.client.login(username=user.username, password=user.raw_password)
+        self.assertTrue(logged_in)
+
+    def url_resolver(self):
+        return reverse(self.url_name)
+
+    def check_responses(self, url):
+        if self.login_required:
+            self.login()
+        for method, code in self.responses:
+            response = getattr(self.client, method)(url)
+            self.assertEqual(response.status_code, code)
+
+    def check_login_required(self, url):
+        self.client.logout()
+        for method, code in self.responses:
+            response = getattr(self.client, method)(url)
+            self.assertRedirects(response, settings.LOGIN_URL + '?next=' + url)
+
+    def test_basics(self):
+        url = self.url_resolver()
+        if self.responses:
+            self.check_responses(url)
+        if self.login_required:
+            self.check_login_required(url)
+
+
+class TestIndex(BasicPageTest, TestCase):
+    url_name = 'index'
+    responses = (('get', 200),)
+    login_required = True
