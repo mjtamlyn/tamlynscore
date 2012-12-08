4d23eb9c0df7c0f39ce0ff1a7b9b9b5c982bd234
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-12-08 16:12:55 +0000

    Remove some pointless variables.

--- core/views.py
+++ core/views.py
@@ -1,22 +1,23 @@
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView, ListView, DetailView, UpdateView
 
+from scoring.utils import class_view_decorator
+
 from .forms import ArcherUpdateForm
 from .models import Club, Archer
 
-from scoring.utils import class_view_decorator
 
 @class_view_decorator(login_required)
 class Index(TemplateView):
     template_name = 'index.html'
 
+
 class ClubList(ListView):
     model = Club
-club_list = login_required(ClubList.as_view())
+
 
 class ClubDetail(DetailView):
     model = Club
-club_detail = login_required(ClubDetail.as_view())
 
 
 class ArcherUpdate(UpdateView):
