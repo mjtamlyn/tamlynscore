1453f96a057c7ca69c357d0d808e523358220472
Marc Tamlyn <marc.tamlyn@gmail.com>
2013-03-12 23:44:41 +0000

    Tidy up urls, remove some unused imports.

--- scoring/urls.py
+++ scoring/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -8,9 +8,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'', include('core.urls')),
-    (r'^tournaments/entries/', include('entries.urls')),
-    (r'^tournaments/scoring/', include('scores.urls')),
-    (r'^tournaments/olympic/', include('olympic.urls')),
+    (r'^tournaments/', include('entries.urls')),
+    (r'^tournaments/', include('scores.urls')),
+    (r'^tournaments/', include('olympic.urls')),
     (r'^accounts/', include('accounts.urls')),
     (r'^admin/', include(admin.site.urls)),
 )
