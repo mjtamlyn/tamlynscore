24af8afd40bfc829930534a320b2905b6c701a38
Marc Tamlyn <marc.tamlyn@gmail.com>
2014-02-23 09:37:06 +0000

    Add first version of entry add form.

--- entries/forms.py
+++ entries/forms.py
@@ -1,4 +1,13 @@
+from django.db import transaction
 from django import forms
+
+from core.models import Archer
+from .models import CompetitionEntry, SessionEntry
+
+
+class SessionChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return unicode(obj.shot_round)
 
 
 class EntrySearchForm(forms.Form):
@@ -6,4 +15,47 @@ class EntrySearchForm(forms.Form):
 
 
 class EntryCreateForm(forms.Form):
-    pass
+    archer = forms.ModelChoiceField(Archer.objects)
+    guest = forms.BooleanField(required=False)
+
+    def __init__(self, competition, **kwargs):
+        self.competition = competition
+        super(EntryCreateForm, self).__init__(**kwargs)
+        self.session_fields = []
+        for session in self.competition.session_set.order_by('start').prefetch_related('sessionround_set'):
+            field = SessionChoiceField(session.sessionround_set.all(), required=False, label=session.start)
+            field.key = 'session_%s' % session.pk
+            self.fields[field.key] = field
+            self.session_fields.append(field)
+
+    def clean(self):
+        data = self.cleaned_data
+        if 'archer' in data:
+            archer = data['archer']
+            for field in ['bowstyle', 'club', 'age', 'novice']:
+                if field not in data:
+                    self.cleaned_data[field] = getattr(archer, field)
+        if self.session_fields:
+            if not any(data[field.key] for field in self.session_fields):
+                raise forms.ValidationError('Must enter at least one session')
+        return self.cleaned_data
+
+    def save(self):
+        data = self.cleaned_data
+        with transaction.atomic():
+            competition_entry = CompetitionEntry.objects.create(
+                competition=self.competition,
+                archer=data['archer'],
+                club=data['club'],
+                bowstyle=data['bowstyle'],
+                age=data['age'],
+                novice=data['novice'],
+                guest=data['guest'],
+            )
+            for field in self.session_fields:
+                if data[field.key]:
+                    SessionEntry.objects.create(
+                        competition_entry=competition_entry,
+                        session_round=data[field.key],
+                    )
+        return competition_entry
--- tests/test_entry_system.py
+++ tests/test_entry_system.py
@@ -5,7 +5,8 @@ from django.core.urlresolvers import reverse, resolve
 from django.test import TestCase, RequestFactory
 
 from entries.forms import EntrySearchForm, EntryCreateForm
-from entries.views import EntryList
+from entries.models import CompetitionEntry, SessionEntry
+from entries.views import EntryList, EntryAdd
 
 from . import factories
 
@@ -77,3 +78,121 @@ class TestEntryList(TestCase):
         request = self.rf.get('/', {'search': 'steve'})
         response = view(request, slug=self.competition.slug)
         self.assertEqual(list(response.context_data['object_list']), [steve.competition_entry, bob.competition_entry])
+
+
+class TestEntryAdd(TestCase):
+    def setUp(self):
+        self.competition = factories.CompetitionFactory.create()
+        self.user = factories.UserFactory.create()
+        self.rf = LoggedInRequestFactory(self.user)
+
+    def test_simple(self):
+        view = EntryAdd.as_view()
+        request = self.rf.get('/')
+        response = view(request, slug=self.competition.slug)
+        self.assertEqual(response.status_code, 200)
+        response.render()
+
+    def test_context(self):
+        view = EntryAdd.as_view()
+        request = self.rf.get('/')
+        response = view(request, slug=self.competition.slug)
+        context = response.context_data
+        self.assertIsInstance(context['form'], EntryCreateForm)
+
+    def test_auth(self):
+        view = EntryAdd.as_view()
+        request = self.rf.get('/')
+        request.user = AnonymousUser()
+        response = view(request, slug=self.competition.slug)
+        self.assertEqual(response.status_code, 302)
+
+    def test_reversal(self):
+        url = reverse('entry_add', kwargs={'slug': self.competition.slug})
+        match = resolve(url)
+        self.assertEqual(match.func.__name__, 'EntryAdd')
+
+    def test_post(self):
+        archer = factories.ArcherFactory.create()
+        session_round = factories.SessionRoundFactory.create(session__competition=self.competition)
+        request = self.rf.post('/', {
+            'archer': archer.pk,
+            'session_%s' % session_round.session_id: session_round.pk,
+        })
+        view = EntryAdd.as_view()
+        response = view(request, slug=self.competition.slug)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(CompetitionEntry.objects.count(), 1)
+
+
+class TestEntryCreateForm(TestCase):
+    def setUp(self):
+        self.competition = factories.CompetitionFactory.create()
+
+    def test_simple(self):
+        archer = factories.ArcherFactory.create()
+        session_round = factories.SessionRoundFactory.create(session__competition=self.competition)
+        data = {
+            'archer': archer.pk,
+            'session_%s' % session_round.session.pk: session_round.pk,
+        }
+        form = EntryCreateForm(competition=self.competition, data=data)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(CompetitionEntry.objects.count(), 1)
+        self.assertEqual(SessionEntry.objects.count(), 1)
+
+    def test_fails_with_no_archer(self):
+        form = EntryCreateForm(competition=self.competition, data={})
+        self.assertFalse(form.is_valid())
+        self.assertTrue('archer' in form.errors)
+
+    def test_fails_with_no_sessions(self):
+        archer = factories.ArcherFactory.create()
+        factories.SessionRoundFactory.create(session__competition=self.competition)
+        data = {
+            'archer': archer.pk,
+        }
+        form = EntryCreateForm(competition=self.competition, data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_not_all_sessions_required(self):
+        archer = factories.ArcherFactory.create()
+        session_round = factories.SessionRoundFactory.create(session__competition=self.competition)
+        factories.SessionRoundFactory.create(session__competition=self.competition)
+        data = {
+            'archer': archer.pk,
+            'session_%s' % session_round.session.pk: session_round.pk,
+        }
+        form = EntryCreateForm(competition=self.competition, data=data)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(CompetitionEntry.objects.count(), 1)
+        self.assertEqual(SessionEntry.objects.count(), 1)
+
+    def test_multiple_sessions(self):
+        archer = factories.ArcherFactory.create()
+        session_round = factories.SessionRoundFactory.create(session__competition=self.competition)
+        session_round_2 = factories.SessionRoundFactory.create(session__competition=self.competition)
+        data = {
+            'archer': archer.pk,
+            'session_%s' % session_round.session.pk: session_round.pk,
+            'session_%s' % session_round_2.session.pk: session_round_2.pk,
+        }
+        form = EntryCreateForm(competition=self.competition, data=data)
+        self.assertTrue(form.is_valid())
+        form.save()
+        self.assertEqual(CompetitionEntry.objects.count(), 1)
+        self.assertEqual(SessionEntry.objects.count(), 2)
+
+    def test_fields(self):
+        form = EntryCreateForm(competition=self.competition)
+        self.assertEqual(form.fields.keys(), ['archer', 'guest'])
+
+    def test_session_fields(self):
+        sr1 = factories.SessionRoundFactory.create(session__competition=self.competition)
+        sr2 = factories.SessionRoundFactory.create(session=sr1.session)
+        sr3 = factories.SessionRoundFactory.create(session__competition=self.competition)
+        form = EntryCreateForm(competition=self.competition)
+        self.assertEqual(list(form.fields['session_%s' % sr1.session_id].queryset), [sr1, sr2])
+        self.assertEqual(list(form.fields['session_%s' % sr3.session_id].queryset), [sr3])
