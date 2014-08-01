22a80a6c9877ae4b1e972e6b6eabb0ce359e8bcc
Marc Tamlyn <marc.tamlyn@gmail.com>
2014-08-01 14:03:29 +0100

    Update make matches for OA 2014.

--- olympic/fixtures/make_matches.py
+++ olympic/fixtures/make_matches.py
@@ -1,43 +1,34 @@
 from olympic.models import OlympicSessionRound
 
 
-# BUCS 2014
-gr, lr, l, b, c = OlympicSessionRound.objects.all()[16:]
+# OA 2014
+gr, lr, gc, lc = OlympicSessionRound.objects.all()[21:]
 
 gr.match_set.all().delete()
 lr.match_set.all().delete()
-b.match_set.all().delete()
-l.match_set.all().delete()
-c.match_set.all().delete()
+gc.match_set.all().delete()
+lc.match_set.all().delete()
 
-gr.make_matches(level=7, start=7, timing=1)
-gr.make_matches(level=6, start=39, timing=2)
-gr.make_matches(level=5, start=43, timing=3)
-gr.make_matches(level=4, start=35, timing=4)
-gr.make_matches(level=3, start=21, timing=5)
-gr.make_matches(level=2, start=21, timing=6, expanded=True)
-gr.make_matches(level=1, start=21, timing=7, expanded=True)
+gr.make_matches(level=6, start=5, timing=1, half_only=True)
+gr.make_matches(level=5, start=5, timing=2)
+gr.make_matches(level=4, start=13, timing=4)
+gr.make_matches(level=3, start=17, timing=5)
+gr.make_matches(level=2, start=17, timing=6, expanded=True)
+gr.make_matches(level=1, start=17, timing=7, expanded=True)
 
-lr.make_matches(level=6, start=7, timing=2)
-lr.make_matches(level=5, start=27, timing=3)
-lr.make_matches(level=4, start=27, timing=4)
-lr.make_matches(level=3, start=17, timing=5)
-lr.make_matches(level=2, start=17, timing=6, expanded=True)
-lr.make_matches(level=1, start=17, timing=7, expanded=True)
+lr.make_matches(level=5, start=5, timing=3)
+lr.make_matches(level=4, start=5, timing=4)
+lr.make_matches(level=3, start=13, timing=5)
+lr.make_matches(level=2, start=13, timing=6, expanded=True)
+lr.make_matches(level=1, start=13, timing=7, expanded=True)
 
-b.make_matches(level=5, start=9, timing=3)
-b.make_matches(level=4, start=17, timing=4)
-b.make_matches(level=3, start=13, timing=5)
-b.make_matches(level=2, start=13, timing=6, expanded=True)
-b.make_matches(level=1, start=13, timing=7, expanded=True)
+gc.make_matches(level=5, start=21, timing=3)
+gc.make_matches(level=4, start=21, timing=4)
+gc.make_matches(level=3, start=21, timing=5)
+gc.make_matches(level=2, start=21, timing=6, expanded=True)
+gc.make_matches(level=1, start=21, timing=7, expanded=True)
 
-l.make_matches(level=4, start=9, timing=4)
-l.make_matches(level=3, start=9, timing=5)
-l.make_matches(level=2, start=9, timing=6, expanded=True)
-l.make_matches(level=1, start=9, timing=7, expanded=True)
-
-c.make_matches(level=5, start=1, timing=3, half_only=True)
-c.make_matches(level=4, start=1, timing=4)
-c.make_matches(level=3, start=5, timing=5)
-c.make_matches(level=2, start=5, timing=6, expanded=True)
-c.make_matches(level=1, start=5, timing=7, expanded=True)
+lc.make_matches(level=4, start=29, timing=4)
+lc.make_matches(level=3, start=25, timing=5)
+lc.make_matches(level=2, start=25, timing=6, expanded=True)
+lc.make_matches(level=1, start=25, timing=7, expanded=True)
