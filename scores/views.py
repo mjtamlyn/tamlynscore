7bd17162b749d7ae00e93380da98ef1380db40f7
Marc Tamlyn <marc.tamlyn@gmail.com>
2014-08-01 20:19:16 +0100

    Soften a check.

--- scores/views.py
+++ scores/views.py
@@ -907,7 +907,7 @@ class NewLeaderboard(PDFResultsRenderer, CSVResultsRenderer, ListView):
             for category in results[section]:
                 for score in results[section][category]:
                     score.details = self.mode.score_details(score, section)
-                    if getattr(score, 'team'):
+                    if getattr(score, 'team', None):
                         for archer in score.team:
                             archer.details = self.mode.score_details(archer, section)
         return super(NewLeaderboard, self).render_to_response(context, **response_kwargs)
