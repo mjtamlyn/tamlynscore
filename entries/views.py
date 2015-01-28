4eb29a72f2829bf03ae6a596a9e440015a90eaee
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-01-28 19:54:31 +0000

    Add archer search form to the entry list page.

--- entries/views.py
+++ entries/views.py
@@ -123,6 +123,7 @@ class EntryList(CompetitionMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super(EntryList, self).get_context_data(**kwargs)
         context['stats'] = self.get_stats()
+        context['search_form'] = ArcherSearchForm()
         return context
 
 
