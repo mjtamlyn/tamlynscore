24bc8d47004e0a17a2d7f4943337dc416dd6afde
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-01-28 09:30:32 +0000

    Update dependencies, including Django 1.7.

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -1,127 +1,139 @@
-# encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-        
-        # Adding model 'Subround'
-        db.create_table('core_subround', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('arrows', self.gf('django.db.models.fields.PositiveIntegerField')()),
-            ('distance', self.gf('django.db.models.fields.PositiveIntegerField')()),
-            ('unit', self.gf('django.db.models.fields.CharField')(max_length=1)),
-            ('target_face', self.gf('django.db.models.fields.PositiveIntegerField')()),
-        ))
-        db.send_create_signal('core', ['Subround'])
-
-        # Adding model 'Round'
-        db.create_table('core_round', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
-            ('scoring_type', self.gf('django.db.models.fields.CharField')(max_length=1)),
-        ))
-        db.send_create_signal('core', ['Round'])
-
-        # Adding M2M table for field subrounds on 'Round'
-        db.create_table('core_round_subrounds', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('round', models.ForeignKey(orm['core.round'], null=False)),
-            ('subround', models.ForeignKey(orm['core.subround'], null=False))
-        ))
-        db.create_unique('core_round_subrounds', ['round_id', 'subround_id'])
-
-        # Adding model 'Bowstyle'
-        db.create_table('core_bowstyle', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=50)),
-        ))
-        db.send_create_signal('core', ['Bowstyle'])
-
-        # Adding model 'Club'
-        db.create_table('core_club', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=500)),
-            ('short_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
-        ))
-        db.send_create_signal('core', ['Club'])
-
-        # Adding model 'Archer'
-        db.create_table('core_archer', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('gender', self.gf('django.db.models.fields.CharField')(max_length=1)),
-            ('club', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Club'])),
-            ('bowstyle', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Bowstyle'])),
-            ('age', self.gf('django.db.models.fields.CharField')(max_length=1)),
-            ('novice', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal('core', ['Archer'])
+from django.db import models, migrations
 
 
-    def backwards(self, orm):
-        
-        # Deleting model 'Subround'
-        db.delete_table('core_subround')
+class Migration(migrations.Migration):
 
-        # Deleting model 'Round'
-        db.delete_table('core_round')
+    dependencies = [
+    ]
 
-        # Removing M2M table for field subrounds on 'Round'
-        db.delete_table('core_round_subrounds')
-
-        # Deleting model 'Bowstyle'
-        db.delete_table('core_bowstyle')
-
-        # Deleting model 'Club'
-        db.delete_table('core_club')
-
-        # Deleting model 'Archer'
-        db.delete_table('core_archer')
-
-
-    models = {
-        'core.archer': {
-            'Meta': {'object_name': 'Archer'},
-            'age': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
-            'bowstyle': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Bowstyle']"}),
-            'club': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Club']"}),
-            'gender': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'novice': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
-        },
-        'core.bowstyle': {
-            'Meta': {'object_name': 'Bowstyle'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'})
-        },
-        'core.club': {
-            'Meta': {'object_name': 'Club'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '500'}),
-            'short_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
-        },
-        'core.round': {
-            'Meta': {'object_name': 'Round'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
-            'scoring_type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
-            'subrounds': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.Subround']", 'symmetrical': 'False'})
-        },
-        'core.subround': {
-            'Meta': {'object_name': 'Subround'},
-            'arrows': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'distance': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'target_face': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'unit': ('django.db.models.fields.CharField', [], {'max_length': '1'})
-        }
-    }
-
-    complete_apps = ['core']
+    operations = [
+        migrations.CreateModel(
+            name='Archer',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('gender', models.CharField(max_length=1, choices=[(b'G', b'Gent'), (b'L', b'Lady')])),
+                ('age', models.CharField(default=b'S', max_length=1, choices=[(b'J', b'Junior'), (b'S', b'Senior')])),
+                ('novice', models.CharField(default=b'E', max_length=1, choices=[(b'N', b'Novice'), (b'E', b'Experienced')])),
+                ('gnas_no', models.BigIntegerField(null=True, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Bowstyle',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=50)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Club',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=500)),
+                ('short_name', models.CharField(unique=True, max_length=50)),
+                ('abbreviation', models.CharField(default=b'', max_length=10, blank=True)),
+                ('slug', models.SlugField(unique=True, editable=False)),
+            ],
+            options={
+                'ordering': ('short_name',),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Country',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=50)),
+            ],
+            options={
+                'verbose_name_plural': 'countries',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='County',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=50)),
+            ],
+            options={
+                'verbose_name_plural': 'counties',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Region',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=50)),
+                ('country', models.ForeignKey(to='core.Country')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Round',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=100)),
+                ('scoring_type', models.CharField(max_length=1, choices=[(b'F', b'Five Zone Imperial'), (b'T', b'AGB Indoor - Ten Zone, no Xs, with hits'), (b'X', b'WA Outdoor - Ten Zone, with Xs, no hits'), (b'I', b'WA Indoor - Ten Zone, no Xs, no hits'), (b'W', b'Worcester')])),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Subround',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('arrows', models.PositiveIntegerField()),
+                ('distance', models.PositiveIntegerField()),
+                ('unit', models.CharField(max_length=1, choices=[(b'm', b'metres'), (b'y', b'yards')])),
+                ('target_face', models.PositiveIntegerField()),
+            ],
+            options={
+                'ordering': ('unit', '-distance', '-arrows', '-target_face'),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='round',
+            name='subrounds',
+            field=models.ManyToManyField(to='core.Subround'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='county',
+            name='region',
+            field=models.ForeignKey(to='core.Region'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='club',
+            name='county',
+            field=models.ForeignKey(blank=True, to='core.County', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='archer',
+            name='bowstyle',
+            field=models.ForeignKey(to='core.Bowstyle'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='archer',
+            name='club',
+            field=models.ForeignKey(to='core.Club'),
+            preserve_default=True,
+        ),
+    ]
