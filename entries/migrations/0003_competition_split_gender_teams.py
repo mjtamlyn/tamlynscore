af3096a4c1c3bb592036714189d3e8ec26e70af2
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-02-05 20:01:48 +0000

    Add split gender teams flag.
    Fixes #7.

---
+++ entries/migrations/0003_competition_split_gender_teams.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('entries', '0002_auto_20150128_0925'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='competition',
+            name='split_gender_teams',
+            field=models.BooleanField(default=False, help_text=b'Does not affect novice teams'),
+            preserve_default=True,
+        ),
+    ]
