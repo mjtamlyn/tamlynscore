1652de0d6881f084bdd0070fa97218445cf6f063
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-06-08 20:51:54 +0100

    Purge cache for the moment.

--- entries/forms.py
+++ entries/forms.py
@@ -66,8 +66,8 @@ class EntryCreateForm(forms.Form):
                 queryset=self.session_rounds,
                 widget=forms.CheckboxSelectMultiple,
             )
-            if cache.get('entry-form:sessions'):
-                self.initial['sessions'] = cache.get('entry-form:sessions')
+            #if cache.get('entry-form:sessions'):
+            #    self.initial['sessions'] = cache.get('entry-form:sessions')
 
     def get_current_obj(self):
         return self.archer
