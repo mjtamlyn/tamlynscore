5d69642b2efa9ebd5e032130a016d4be033f39f0
David Richardson <david@tankski.co.uk>
2015-10-29 13:18:36 +0000

    Fix formatting

--- core/forms.py
+++ core/forms.py
@@ -4,9 +4,9 @@ from .models import Archer
 
 
 class ArcherForm(forms.ModelForm):
-	def __init__(self, *args, **kwargs):
-		super(ArcherForm, self).__init__(*args, **kwargs)
-		self.fields['name'].widget.attrs.update({'autofocus': ''})
+    def __init__(self, *args, **kwargs):
+        super(ArcherForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'autofocus': ''})
 
     class Meta:
         model = Archer
