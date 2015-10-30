761e3661719864325c6226ec33ee672076155fdb
Marc Tamlyn <marc.tamlyn@gmail.com>
2015-10-30 08:39:39 +0000

    Add password reset templates.
    They use floppyforms which is on another branch at the moment.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 import django.contrib.auth.views as auth_views
 
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout_then_login, name='logout'),
+    url(r'', include('django.contrib.auth.urls')),
 ]
