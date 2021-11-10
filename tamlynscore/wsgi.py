8999e0e92b90a0c3d0d6303f983b452a979e9c2e
Marc Tamlyn <marc.tamlyn@gmail.com>
2021-11-10 13:13:35 +0000

    Remove old wsgi middleware options.

--- tamlynscore/wsgi.py
+++ tamlynscore/wsgi.py
@@ -1,9 +1,6 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tamlynscore.settings")  # NOQA
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
+from django.core.wsgi import get_wsgi_application  # NOQA
 
-from .wsgi_middleware import DomainRedirect
-
-application = DomainRedirect(Cling(MediaCling(get_wsgi_application())))
+application = get_wsgi_application()
