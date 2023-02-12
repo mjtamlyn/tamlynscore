24b8154c73e0a8531a30578bac0fc167a13e895f
Marc Tamlyn <marc.tamlyn@gmail.com>
2023-02-12 22:37:31 +0000

    Try to serve media via Whitenoise WSGI

--- tamlynscore/wsgi.py
+++ tamlynscore/wsgi.py
@@ -3,5 +3,11 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tamlynscore.settings")  # NOQA
 
 from django.core.wsgi import get_wsgi_application  # NOQA
+from django.conf import settings  # NOQA
+from whitenoise import WhiteNoise  # NOQA
 
-application = get_wsgi_application()
+
+application = WhiteNoise(
+    get_wsgi_application(),
+    root=settings.MEDIA_ROOT,
+)
