3f40568dd416bc1fa4427cec4e3dea6603ed6146
Marc Tamlyn <marc.tamlyn@gmail.com>
2023-02-12 22:46:05 +0000

    Isort in wsgi file

--- tamlynscore/wsgi.py
+++ tamlynscore/wsgi.py
@@ -2,10 +2,10 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tamlynscore.settings")  # NOQA
 
-from django.core.wsgi import get_wsgi_application  # NOQA
 from django.conf import settings  # NOQA
-from whitenoise import WhiteNoise  # NOQA
+from django.core.wsgi import get_wsgi_application  # NOQA
 
+from whitenoise import WhiteNoise  # NOQA
 
 application = WhiteNoise(
     get_wsgi_application(),
