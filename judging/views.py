8be51a83bdbbeab3178b9bf9e4db683fee0bb9a4
Marc Tamlyn <marc.tamlyn@gmail.com>
2023-08-06 12:40:35 +0100

    Add more information to judge match display.

--- judging/views.py
+++ judging/views.py
@@ -7,6 +7,7 @@ from braces.views import MessageMixin
 
 from entries.models import SessionEntry
 from entries.views import CompetitionMixin, Registration
+from olympic.models import Seeding
 from olympic.views import FieldPlanMixin
 
 from .models import Judge
@@ -60,6 +61,12 @@ class JudgeMatches(FieldPlanMixin, JudgeMixin, TemplateView):
         if len(matches) == 0:
             return context
 
+        session_rounds = {m.session_round for m in matches}
+        seedings = Seeding.objects.filter(session_round__in=session_rounds)
+        seedings_dict = {(s.session_round_id, s.seed): s for s in seedings}
+
+        max_levels = {session_round: max(m.level for m in matches if m.session_round == session_round) for session_round in session_rounds}
+
         max_timing = (max(matches, key=lambda m: m.timing)).timing
         last_match = max(matches, key=lambda m: (m.target_2 or m.target))
         max_target = last_match.target_2 or last_match.target
@@ -75,20 +82,60 @@ class JudgeMatches(FieldPlanMixin, JudgeMixin, TemplateView):
                 'match': None,
                 'category': None,
                 'round': None,
+                'is_bye': False,
+                'has_second_target': False,
+                'is_second_target': False,
+                'archer_1': None,
+                'archer_2': None,
+                'seed_1': None,
+                'seed_2': None,
             } for i in range(1, max_target + 1)],
         } for letter in passes[:max_timing]]
         for m in matches:
-            layout[m.timing - 1]['targets'][m.target - 1]['match'] = m
-            layout[m.timing - 1]['targets'][m.target - 1]['category'] = m.session_round.category.name
-            layout[m.timing - 1]['targets'][m.target - 1]['distance'] = '%sm' % m.session_round.shot_round.distance
-            layout[m.timing - 1]['targets'][m.target - 1]['round'] = levels[m.level - 1]
+            slot = layout[m.timing - 1]['targets'][m.target - 1]
+            slot['match'] = m
+            slot['category'] = m.session_round.category.name
+            slot['distance'] = '%sm' % m.session_round.shot_round.distance
+            slot['round'] = levels[m.level - 1]
             if m.level == 1 and m.match == 2:
-                layout[m.timing - 1]['targets'][m.target - 1]['round'] = 'Bronze'
+                slot['round'] = 'Bronze'
+            seeds = [m.match, (2 ** m.level) + 1 - m.match]
+            if not m.match % 2:
+                seeds.reverse()
+            if m.level == max_levels[m.session_round] and seedings_dict:
+                seed_1 = seedings_dict.get((m.session_round_id, seeds[0]), None)
+                if seed_1:
+                    slot['seed_1'] = seed_1.seed
+                    slot['archer_1'] = seed_1.entry.archer
+                else:
+                    slot['archer_1'] = 'BYE'
+                seed_2 = seedings_dict.get((m.session_round_id, seeds[1]), None)
+                if seed_2:
+                    slot['seed_2'] = seed_2.seed
+                    slot['archer_2'] = seed_2.entry.archer
+                else:
+                    slot['archer_2'] = 'BYE'
+                if not seed_1 or not seed_2:
+                    slot['is_bye'] = True
+            else:
+                slot['seed_1'] = seeds[0]
+                slot['archer_1'] = 'TBC'
+                slot['seed_2'] = seeds[1]
+                slot['archer_2'] = 'TBC'
+
             if m.target_2:
-                layout[m.timing - 1]['targets'][m.target_2 - 1].update(
-                    layout[m.timing - 1]['targets'][m.target - 1]
-                )
-                layout[m.timing - 1]['targets'][m.target_2 - 1]['number'] = m.target_2
+                slot_2 = layout[m.timing - 1]['targets'][m.target_2 - 1]
+                slot_2['match'] = slot['match']
+                slot_2['category'] = slot['category']
+                slot_2['distance'] = slot['distance']
+                slot_2['round'] = slot['round']
+                slot['has_second_target'] = True
+                slot_2['is_second_target'] = True
+
+                if (not slot['is_bye']) and slot['seed_1'] and slot['seed_2']:
+                    slot_2['archer_1'] = slot['archer_2']
+                    slot_2['seed_1'] = slot['seed_2']
+
         context['layout'] = layout
 
         return context
