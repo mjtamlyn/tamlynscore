1a1ad4f618a242069eef9f527a4440a0eedd8e65
Marc Tamlyn <marc.tamlyn@gmail.com>
2023-12-09 18:02:55 +0000

    Allow toggling between byEntered and byDozen.

--- js_src/input/ScoreSheet.js
+++ js_src/input/ScoreSheet.js
@@ -1,37 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ArcherRowDetails from './ArcherRowDetails';
 import { getEnd, getEndScore, getGoldCount, getRunningTotal } from './utils';
 
 
+const byEntered = 'byEntered';
+const byDozen = 'byDozen';
+
+
 const ScoreSheet = ({ score, toSummary }) => {
-    const ends = [...Array(score.endCount).keys()];
-    const arrowNumbers = [...Array(score.endLength).keys()];
+    const [displayMode, setDisplayMode] = useState(byEntered);
+
+    const totalArrows = score.endCount * score.endLength;
+    const endCount = (displayMode === byEntered) ? score.endCount : (totalArrows / 12);
+    const endLength = (displayMode === byEntered) ? score.endLength : 12;
+
+    let displayToggle = null;
+    if (!(totalArrows % 12) && score.endLength < 12) {
+        const toggleDisplayMode = (e) => {
+            e.preventDefault();
+            if (displayMode === byEntered) {
+                setDisplayMode(byDozen);
+            } else {
+                setDisplayMode(byEntered);
+            }
+        };
+        displayToggle = (
+            <a onClick={ toggleDisplayMode } className="btn btn-small btn-toggle archers__score__toggle">
+                <div className={ 'btn-segment ' + (displayMode === byEntered ? 'selected' : '') }>{ score.endLength }s</div>
+                <div className={ 'btn-segment ' + (displayMode === byDozen ? 'selected' : '') }>12s</div>
+            </a>
+        );
+    }
+
+    const ends = [...Array(endCount).keys()];
+    const arrowNumbers = [...Array(endLength).keys()];
+
     const scoreRows = ends.map((endNumber) => {
-        const end = getEnd(score, endNumber + 1);
-        const arrows = arrowNumbers.map(index => <div key={ `${endNumber}|${index}` }>{ end[index] || '-' }</div>);
+        const end = getEnd(score, endNumber + 1, endLength);
+
+        const arrows = [];
+        if (endLength === 12) {
+            arrows.push(...arrowNumbers.slice(0, 6).map(index => <div key={ `${endNumber}|${index}` }>{ end[index] || '-' }</div>));
+            arrows.push(<div className="archers__score__total" key={ `${endNumber}|end-total-1` }>{ getEndScore(score, (endNumber + 1) * 2 - 1, 6) }</div>)
+            arrows.push(...arrowNumbers.slice(6, 12).map(index => <div key={ `${endNumber}|${index + 6}` }>{ end[index] || '-' }</div>));
+            arrows.push(<div className="archers__score__total" key={ `${endNumber}|end-total-2` }>{ getEndScore(score, (endNumber + 1) * 2, 6) }</div>)
+        } else {
+            arrows.push(...arrowNumbers.map(index => <div key={ `${endNumber}|${index}` }>{ end[index] || '-' }</div>));
+        }
+
         return [
             ...arrows,
-            <div className="archers__score__total" key={ `${endNumber}|end` }>{ getEndScore(score, endNumber + 1) }</div>,
-            <div className="archers__score__total" key={ `${endNumber}|golds` }>{ getGoldCount(score, endNumber + 1) }</div>,
-            <div className="archers__score__total" key={ `${endNumber}|rt` }>{ getRunningTotal(score, endNumber + 1) }</div>,
+            <div className="archers__score__total" key={ `${endNumber}|end` }>{ getEndScore(score, endNumber + 1, endLength) }</div>,
+            <div className="archers__score__total" key={ `${endNumber}|golds` }>{ getGoldCount(score, endNumber + 1, endLength) }</div>,
+            <div className="archers__score__total" key={ `${endNumber}|rt` }>{ getRunningTotal(score, endNumber + 1, endLength) }</div>,
         ];
     });
 
-    const columnsCount = score.endLength + 3;
+    const columnsCount = endLength + 3 + (endLength === 12 ? 2 : 0);
+
+    let arrowsHeading = (
+        <div className="archers__score__heading archers__score__heading--span" style={ { gridColumnEnd: `span ${endLength}` } }>Arrows</div>
+    );
+    if (endLength === 12) {
+        arrowsHeading = <>
+            <div className="archers__score__heading archers__score__heading--span" style={ { gridColumnEnd: `span 6` } }>Arrows</div>
+            <div className="archers__score__heading">ET</div>
+            <div className="archers__score__heading archers__score__heading--span" style={ { gridColumnEnd: `span 6` } }>Arrows</div>
+            <div className="archers__score__heading">ET</div>
+        </>;
+    }
 
     return (
         <>
             <div className="full-height-page__card">
+                { displayToggle && 
+                    <div className="archers__score-display-toggle">
+                        Display in { displayToggle }
+                    </div>
+                }
                 <div className="archers__row">
                     <ArcherRowDetails score={ score } />
                     <div className="archers__score" style={ { gridTemplateColumns: `repeat(${columnsCount}, 1fr)`} }>
-                        <div className="archers__score__heading archers__score__heading--span" style={ { gridColumnEnd: `span ${score.endLength}` } }>Arrows</div>
+                        { arrowsHeading }
                         <div className="archers__score__heading">S</div>
                         <div className="archers__score__heading">10s</div>
                         <div className="archers__score__heading">RT</div>
                         { scoreRows }
-                        <div style={ { gridColumnEnd: `span ${score.endLength}` } } />
+                        <div style={ { gridColumnEnd: `span ${endLength === 12 ? 14 : endLength}` } } />
                         <div className="archers__score__grand-total">
                             { getRunningTotal(score) }
                         </div>
--- js_src/input/utils.js
+++ js_src/input/utils.js
@@ -17,33 +17,33 @@ function currentEnd(score) {
     return Math.floor((score.arrows.length - 1) / score.endLength) + 1;
 };
 
-function getEnd(score, endNumber) {
-    return score.arrows.slice((endNumber - 1) * score.endLength, endNumber * score.endLength);
+function getEnd(score, endNumber, endLength) {
+    return score.arrows.slice((endNumber - 1) * (endLength || score.endLength), endNumber * (endLength || score.endLength));
 }
 
 function isEndComplete(score, endNumber) {
     return getEnd(score, endNumber).length == score.endLength;
 }
 
-function getRunningTotal(score, endNumber) {
+function getRunningTotal(score, endNumber, endLength) {
     if (!endNumber) {
         return sum(score.arrows);
     }
-    return sum(score.arrows.slice(0, endNumber * score.endLength));
+    return sum(score.arrows.slice(0, endNumber * (endLength || score.endLength)));
 }
 
 function getArrowsShot(score) {
     return score.arrows.length;
 }
 
-function getEndScore(score, endNumber) {
-    return sum(getEnd(score, endNumber));
+function getEndScore(score, endNumber, endLength) {
+    return sum(getEnd(score, endNumber, endLength));
 }
 
-function getGoldCount(score, endNumber) {
+function getGoldCount(score, endNumber, endLength) {
     let arrows = score.arrows;
     if (endNumber) {
-        arrows = getEnd(score, endNumber);
+        arrows = getEnd(score, endNumber, endLength);
     }
     return sum(arrows.map(a => ((a === 10) ? 1 : 0)));
 }
